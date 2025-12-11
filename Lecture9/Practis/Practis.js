6558b1aaeb4674d20b6c0176c8db1ac26d4054b9
vishal birla <135350754+vishalbirla44@users.noreply.github.com>
2025-12-11 05:49:41 +0000

    practis set

--- Lecture9/Practis/Practis.js
+++ Lecture9/Practis/Practis.js
@@ -8,7 +8,36 @@ app.use((req,res,next) => {
 })
 app.use((req,res,next) => {
     console.log( "this is second middleware",req.url ,req.method)
+    next()
 })
+// app.use((req,res,next) => {
+//     console.log( "this is thered middleware",req.url ,req.method)
+//     res.send("<h1>this is node page </h1>")
+// })
+
+app.get((req,res,next) => {
+   console.log("this is handle / get method ")
+   res.send(`<h1> this is home page </h1>`)
+})
+
+app.get("/contect-us",(req,res,next)=> {
+  console.log(`this is get for contect us `)
+  res.send(`<h1>file you details </h1>
+    <form action="/contect-us" method="POST">
+     <lable for="name"> Name</lable>
+     <input type= "text" name="username" placeholder="Enter you Name" >
+      <lable for="name"> email</lable>
+     <input type= "text" name="gmail" placeholder="Enter your gmail">
+     <input type="submit">
+    </form>`)
+})
+
+app.post("/contect-us",(req,res,next) => {
+    res.send("<h1>this is post method for form </h1>")
+})
+
+
+
 
 const PORT = 3000
 app.listen(PORT , () => {
--- Lecture9/app.js
+++ Lecture9/app.js
@@ -3,21 +3,25 @@
 const express = require("express")
 
 //  local module 
-const requestHandler = require("./user")
 
 const app = express()
 
-app.use("/",(req,res,next) =>{
+// first middleware
+app.use((req,res,next) =>{
     console.log( "this is first middleware",req.url, req.method)
     next()
 })
  
-app.use("/submit-details",(req,res,next) =>{
+// second middleware
+app.use((req,res,next) =>{
     console.log( "this is second  middleware",req.url, req.method)
-    res.send("<h1> welcome to me </h1>")
+    next()
 })
- 
-
+// therid middleware
+app.use((req,res,next) => {
+    console.log("this is thered middleware ")
+    res.send("<h1> Hello this node learninig page </h1>")
+})
 
 const PORT = 3000
 
