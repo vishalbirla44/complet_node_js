59e6f3ad5b88035de494a55b8e5e56f1912cdd6c
vishal birla <135350754+vishalbirla44@users.noreply.github.com>
2025-12-18 18:20:46 +0000

    final

--- Lecture10-airbnb/routes/hostRouter.js
+++ Lecture10-airbnb/routes/hostRouter.js
@@ -5,15 +5,18 @@ const express = require("express")
 
 const hostRouter = express.Router()
 
+// local module
+const rootDir = require("../utils/pathUtil")
+
 
 hostRouter.get("/add-home", (req,res,next) => {
-    res.sendFile(path.join(__dirname, "../" ,"views" , "addhome.html"))
+    res.sendFile(path.join(  rootDir,"../" ,"views" , "addhome.html"))
 })
 
 
 hostRouter.post("/add-home", (req,res,next) => {
     console.log(req.body)
-     res.sendFile(path.join(__dirname, "../" ,"views" , "homeAdded.html"))
+     res.sendFile(path.join( , "../" ,"views" , "homeAdded.html"))
 })
 
 
